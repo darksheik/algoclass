14f9691fe25681ab06b755ff35b251f59d89006b
Don Pflaster <dpflaster@gmail.com>
2012-06-21 06:16:36 -0400

    Counting inversions through Merge Sort complete

--- inversions.rb
+++ inversions.rb
@@ -4,14 +4,13 @@ class Inversion
 
   def initialize
     @master_array = []
-    @inversions = []
+    @inversioncount = 0
     File.open(ARGV[0]).each_line{ |l| @master_array << l.to_i }
     final = merge_sort(@master_array)
     puts "Original: " + @master_array.to_s
     puts "Size: " + final.length.to_s
     puts "Final: " + final.to_s
-    puts "Inversions: " + @inversions.to_s
-    puts "Inversions count: " + @inversions.length.to_s
+    puts "Inversions count: " + @inversioncount.to_s
   end
 
   private
@@ -23,18 +22,13 @@ class Inversion
     middle = (m.length / 2).to_i
     (0..middle-1).each{ |x| left << m[x] }
     (middle..m.length-1).each{ |x| right << m[x] }
-    #left.each do |lcheck|
-      #right.each do |rcheck|
-        #if lcheck > rcheck
-          #@inversions << [lcheck,rcheck]
-        #end
-      #end
-    #end
-    left = merge_sort(left)
-    right = merge_sort(right)
+    sortedleft = merge_sort(left)
+    sortedright = merge_sort(right)
+    
     # merge the sublists returned from prior calls to merge_sort()
     # and return the resulting merged sublist
-    merged = merge_lists(left, right)
+    merged = merge_lists(sortedleft,sortedright)
+    
     return merged
   end
 
@@ -42,13 +36,17 @@ class Inversion
     list = []
     lptr = 0
     rptr = 0
+    # Count split inversions
     while (lptr < l.length || rptr < r.length) do
       if l[lptr] && r[rptr] && l[lptr] < r[rptr]
         list << l[lptr]
         lptr += 1
       elsif r[rptr]
         if r[rptr] && l[lptr]
-          @inversions << [l[lptr],r[rptr]]
+          # Inversions are counted here
+          # Any items remaining in the left array are the number of inversions present
+          # Merge sort does this automagically while merging, all you have to do is count
+          @inversioncount += l.length - lptr
         end
         list << r[rptr]
         rptr += 1
