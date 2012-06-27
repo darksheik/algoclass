72b414829ecdf6fbe2afcf368ffa907f32636cb7
Don Pflaster <dpflaster@gmail.com>
2012-06-27 07:46:13 -0400

    WIP Quicksort Ruby script

---
+++ 2 - quicksort/quicksort.rb
@@ -0,0 +1,36 @@
+def partition(arr,start,final)
+
+  i = start
+  j = i+1
+
+  if(final-start >= 1)
+	  puts "Considering start through final:  " + arr[start..final].to_s
+
+    puts "Pivot #{i}"
+    while (j <= final) 
+      puts "Comparing #{arr[j]} < #{arr[start]}"
+      if arr[j] && (arr[j] < arr[start])
+        arr[i+1,j] = arr[j,i+1]
+        i += 1
+        j += 1
+      else
+        j += 1
+      end
+      puts arr.to_s
+    end
+  
+    puts "Moving pivot to #{i}"
+    arr[start,i] = arr[i,start]
+    puts arr.to_s
+
+    puts "Recursion small set $start " + (i-1).to_s
+    partition(arr,start,i-1)
+    puts "Recursion large set " + (i+1).to_s + " final:" 
+    partition(arr,i+1,final)
+    return start
+  end
+end
+
+x = [108000,1080123,15,87,34,343,333,12504,1,54,1800,6,10,13,5,8,3,2,11,334,2222,212,157344,22222,3333,12345,87581]
+
+pivot1 = partition(x,0,x.length-1)
